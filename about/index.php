910e2e53a39265cd082479bc7a3d4859d9fc16f7
Jono Brandel <jonobrandel@google.com>
2012-08-30 18:02:24 +0900

    fleshing out about page.

--- about/index.php
+++ about/index.php
@@ -10,6 +10,7 @@
 
 ?>
 
+<!-- <img src="../images/portrait.jpg" alt="__Diminishing Returns__ _2012_" /> -->
 <section>
   <p>
     Jono Brandel (b. May 22, 1986) relies on the combination of two fundamental disciplines: graphic design and computer programming. The results of this mixture vary in form, but usually have a screen-based component. He aims to humanize digital aesthetics.
@@ -21,6 +22,76 @@
     For a full list of performances, commercial work, or CV please email directly.
   </p>
 </section>
+
+<h2 style="height: 0; line-height: 0; padding: 0;">&nbsp;</h2>
+
+<dl>
+  <dt>
+    Experience
+  </dt>
+  <dt>
+    <a href="http://google.com">Google, Inc.</a> <i>2010&mdash;Present</i>
+  </dt>
+  <dd>
+    Design / Development
+  </dd>
+  <dt>
+    <a href="http://fabrica.it">Fabrica</a> <i>2010</i>
+  </dt>
+  <dd>
+    Grant Holder
+  </dd>
+  <dt>
+    <a href="http://prologue.com">Prologue Films</a> <i>2010</i>
+  </dt>
+  <dd>
+    Design
+  </dd>
+  <dt>
+    <a href="http://chela.org.ar">cheLA</a> <i>2008&mdash;2009</i>
+  </dt>
+  <dd>
+    Artist in Residence
+  </dd>
+</dl>
+
+<dl>
+  <dt>
+    Affiliation
+  </dt>
+  <dt>
+    Ghostly International
+  </dt>
+  <dt>
+    Creator&apos;s Project
+  </dt>
+  <dt>
+    Gray Area Foundation for the Arts
+  </dt>
+  <dt>
+    Hardfest
+  </dt>
+</dl>
+
+<dl>
+  <dt>
+    Network
+  </dt>
+  <dt>
+    <a href="http://twitter.com/jonobr1">Twitter</a>
+  </dt>
+  <dt>
+    <a href="http://vimeo.com/jonobr1">vimeo</a>
+  </dt>
+  <dt>
+    <a href="http://behance.com/jonobr1">Behance</a>
+  </dt>
+  <dt>
+    <a href="http://cargocollective.com/jonobr1/following">Cargo</a>
+  </dt>
+</dl>
+
+<section></section>
 
 <?
 
